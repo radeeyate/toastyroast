e7ee506b36d9709020cb4bef86e79a0288ce8f1c
radeeyate <124477460+radeeyate@users.noreply.github.com>
2023-12-17 15:21:33 -0700

    Initial Commit

---
+++ config.py
@@ -0,0 +1,62 @@
+import os
+import dotenv
+import google.generativeai as genai
+from pymongo import MongoClient
+
+dotenv.load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_KEY"))
+client = MongoClient(os.getenv("CONNECTION_STRING"))
+db = client["toastyroast"]
+coll = db["roasts"]
+
+generation_config = {
+    "temperature": 0.70,
+    "top_p": 1,
+    "top_k": 32,
+    "max_output_tokens": 4096,
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+]
+
+model = genai.GenerativeModel(
+    model_name="gemini-pro-vision",
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+)
+
+prompt = """
+Hey Gemini, let\'s RoastyToast this pic!
+Analyze the image: Tell me everything you see. Clothes, vibes, background, what\'s going on?
+Rate their Rizz: Is it on fire, lukewarm, or non-existent? Give me a number (1-10) and explain why.
+Roast their style: Are they a fashion icon or a walking cringe compilation? Be ruthless but funny.
+Humor check: Do they seem chill and relatable, or do they radiate awkwardness? Use memes and current references if you can.
+Bonus points: Surprise me with a witty or unexpected roast that ties everything together. Go for the burn!
+**Remember, I want this roast to be hilarious, personalized, and relevant to the image. Be creative, Gemini, and show me your roasting skills!
+Additional Notes:
+Please do not use pronous such as "she", "her", "he", "him", etc. Refer directly to the person saying pronouns like "you."
+Please do not use terms like "boy," "girl," "man," or "woman," to avoid misgendering the person.
+Please return a response in this format:
+
+Clothes: <rating>/10. <explanation here>.
+Vibes: <rating>/10. <explanation here>.
+Background: <rating>/10. <explanation here>.
+Rizz: <rating>/10. <explanation here>.
+Style: <rating>/10. <explanation here>.
+Humor check: <rating>/10. <explanation here>.
+Caption: <caption to describe image>
+Bonus points: <bonus points (max is 3)>/3. <reason for bonus points.>
+Overall: <your overall thoughts. do NOT provide a rating, as I will sum them up.>
+Improvement Tips: <tips they can use to improve. provide them on the same line, and do not point them on a new line.>
+"""
---
+++ main.py
@@ -0,0 +1,129 @@
+from fastapi import FastAPI, UploadFile, HTTPException, Request, File, Response
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+import secrets
+import google.generativeai as genai
+from pathlib import Path
+from config import *
+from statistics import mean
+from bson import ObjectId
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/generate")
+async def upload_image(request: Request, image: UploadFile = File(...)):
+    if not image.filename.endswith(".jpg") and not image.filename.endswith(".jpeg"):
+        raise HTTPException(400, "Invalid image format. Only JPG/JPEG is accepted.")
+    image_parts = [
+        {"mime_type": "image/jpeg", "data": image.file.read()},
+    ]
+    prompt_parts = [
+        prompt,
+        image_parts[0],
+    ]
+    response = model.generate_content(prompt_parts)
+    print(response.prompt_feedback)
+    if "block_reason" in response.prompt_feedback:
+        return templates.TemplateResponse("error.html", {"request": request})
+    print(response.text)
+    for line in response.text.splitlines():
+        if not line == "":
+            line = line.strip()
+            if line.startswith("Clothes: "):
+                line = line.split(": ")
+                clothesRating = line[1].split("/10. ")[0]
+                clothesReason = line[1].split("/10. ")[1]
+            elif line.startswith("Vibes: "):
+                line = line.split(": ")
+                vibeRating = line[1].split("/10. ")[0]
+                vibeReason = line[1].split("/10. ")[1]
+            elif line.startswith("Background: "):
+                line = line.split(": ")
+                bgRating = line[1].split("/10. ")[0]
+                bgReason = line[1].split("/10. ")[1]
+            elif line.startswith("Rizz: "):
+                line = line.split(": ")
+                rizzRating = line[1].split("/10. ")[0]
+                rizzReason = line[1].split("/10. ")[1]
+            elif line.startswith("Style: "):
+                line = line.split(": ")
+                styleRating = line[1].split("/10. ")[0]
+                styleReason = line[1].split("/10. ")[1]
+            elif line.startswith("Humor check: "):
+                line = line.split(": ")
+                humorRating = line[1].split("/10. ")[0]
+                humorReason = line[1].split("/10. ")[1]
+            elif line.startswith("Caption: "):
+                line = line.split(": ")
+                caption = line[1]
+            elif line.startswith("Bonus points: "):
+                line = line.split(": ")
+                bonusPoints = line[1].split("/3. ")[0]
+                bonusPointsReason = line[1].split("/3. ")[1]
+            elif line.startswith("Overall: "):
+                line = line.split(": ")
+                overallReason = line[1]
+            elif line.startswith("Improvement Tips: "):
+                line = line.split(": ")
+                improvementTips = line[1]
+
+    roast = coll.insert_one(
+        {
+            "overallRating": round(
+                mean(
+                    [
+                        int(rizzRating),
+                        int(clothesRating),
+                        int(vibeRating),
+                        int(bgRating),
+                        int(styleRating),
+                        int(humorRating),
+                    ]
+                )
+            )
+            + int(bonusPoints),
+            "overall": overallReason,
+            "rizzRating": rizzRating,
+            "rizzReason": rizzReason,
+            "clothesRating": clothesRating,
+            "clothesReason": clothesReason,
+            "vibeRating": vibeRating,
+            "vibeReason": vibeReason,
+            "bgRating": bgRating,
+            "bgReason": bgReason,
+            "styleRating": styleRating,
+            "styleReason": styleReason,
+            "humorRating": humorRating,
+            "humorReason": humorReason,
+            "bonusPoints": bonusPoints,
+            "bonusPointsReason": bonusPointsReason,
+            "improvementTips": improvementTips,
+        }
+    )
+    return RedirectResponse(f"/roast/{roast.inserted_id}", status_code=302)
+
+
+@app.get("/roast/{roastID}")
+async def roast(request: Request, roastID: str):
+    dbRoast = coll.find_one({"_id": ObjectId(roastID)})
+    if dbRoast:
+        return templates.TemplateResponse(
+            "rating.html", {"request": request, "roast": dbRoast}
+        )
+    else:
+        return templates.TemplateResponse("notfound.html", {"request": request})
+
+@app.get("/faq")
+async def faq(request: Request):
+    return templates.TemplateResponse("faq.html", {"request": request})
