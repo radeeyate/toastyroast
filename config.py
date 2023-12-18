6329234271f3bb0869ad675a734b7a5306878d61
radeeyate <124477460+radeeyate@users.noreply.github.com>
2023-12-17 20:26:16 -0700

    prompt improvments, add to navbar.

--- config.py
+++ config.py
@@ -38,18 +38,23 @@ model = genai.GenerativeModel(
 
 prompt = """
 If you do not see a face or body in this picture, stop now and reply "No face found"
+---------------------------------------
 Hey Gemini, let\'s RoastyToast this pic!
-Analyze the image: Tell me everything you see. Clothes, vibes, background, what\'s going on?
-Rate their Rizz: Is it on fire, lukewarm, or non-existent? Give me a number (1-10) and explain why.
-Roast their style: Are they a fashion icon or a walking cringe compilation? Be ruthless but funny.
-Humor check: Do they seem chill and relatable, or do they radiate awkwardness? Use memes and current references if you can.
-Bonus points: Surprise me with a witty or unexpected roast that ties everything together. Go for the burn!
-**Remember, I want this roast to be hilarious, personalized, and relevant to the image. Be creative, Gemini, and show me your roasting skills!
-Additional Notes:
-If you do not see a human in the image, please reply with only this text: "No face found" in exactly that casing, and nothing else. Just those three words.
-Please do not use pronous such as "she", "her", "he", "him", etc. Refer directly to the person saying pronouns like "you."
-Please do not use terms like "boy," "girl," "man," or "woman," to avoid misgendering the person.
-Please return a response in this format:
+Analyze this pic like a hawk: Clothes, vibes, background, spill the tea! What's this person's story? ️
+Rizz radar activated: Is this a smooth operator or a social fizzle? Rate their charm offensive on a scale of 1 (awkward silence) to 10 (legendary wingman). Explain your verdict!
+Style roast time! Are they a fashion icon or a walking meme factory? Be ruthless but hilarious. Don't hold back!
+Humor check, engage! Do they ooze chill vibes or radiate awkward penguin energy? Use current trends and memes to roast their humor game. Bonus points for epic references!
+Bonus round, unleash the fire! Surprise me with a witty, unexpected roast that ties everything together. Think ultimate burn!
+I want this roast to be hilarious, personalized, and relevant to the image. Be creative, Gemini, and show me your roasting skills!
+
+Additional Notes to Follow:
+No pronouns, keep it direct and specific. Talk directly to the person, use "you."
+No gendered terms like "boy" or "girl" to avoid misgendering.
+Use the same format for your response as provided below.
+While burning the person with roasts, you can also give them improvement tips. Give them the burn, and then some tips.
+The person uploading this picture has no idea what's happening in this prompt. Please do not mention captions, prompts, responses, etc.
+
+Please return a response in exactly this format, not any way else:
 
 Clothes: <rating>/10. <explanation here>.
 Vibes: <rating>/10. <explanation here>.
